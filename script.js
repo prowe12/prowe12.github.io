bd16f5fbd3f66b1635fa5c409cbc6fcc0c801474
prowe <prowe@harbornet.com>
2022-03-11 11:45:53 -0800

    migrating from other git repo

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+ * Put the nav bar html text into the HTML
+ */
+navHtmlText = '<a class="active" href="index.html">Home</a> ' +
+              '<a href="sudoku.html">Sudoku</a> ' +
+              '<a href="connectFour.html">Connect Four</a>'
+document.getElementById("nav").innerHTML = navHtmlText;
