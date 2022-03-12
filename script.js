ad1c1d607b7a40baf339b4b44f797d875a42ec75
prowe <prowe@harbornet.com>
2022-03-11 21:51:42 -0800

    Changed play buttons from forms to links with buttons in them b/c simpler and b/c post can't run on gitHub

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ navHtmlText = '<a class="active" href="index.html">Home</a> ' +
               '<a href="sudoku.html">Sudoku</a> ' +
               '<a href="connectFour.html">Connect Four</a>'
 document.getElementById("nav").innerHTML = navHtmlText;
+
+document.getElementById("playSudoku").addEventListener("click", function() {
+    location.href = "sudoku.html"});
