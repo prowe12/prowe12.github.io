d07c642a6a06effe816eb61407bee3afa05109f7
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-13 12:49:31 -0700

    Add research website

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ let navHtmlText = '<ul>' +
     '<li><a href="research.html">Research</a>' +
     '<li><a href="projects.html">Projects</a></li>' +
     '<li><a href="datasets.html">Datasets</a>' +
-    '<li><a href="educational.html">Educational</a></li>' +
-    '<li><a href="cv.html">CV</a></li>' +
+    '<li><a href="CV_Rowe_med.pdf">CV</a></li>' +
     '<li><a href="publications.html">Publications</a></li>' +
     '</ul>'
 document.getElementById("nav").innerHTML = navHtmlText;
