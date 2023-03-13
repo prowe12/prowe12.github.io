26ca940d4b57495186d3b9bf726cdc5a7e17099d
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-13 16:39:47 -0700

    Add remaining penguin high pages

---
+++ penguin_high/script.js
@@ -0,0 +1,19 @@
+// @ts-check
+/*
+ Code to be used with all files in web page
+*/
+
+
+/**
+ * Put the nav bar html text into the HTML
+ */
+let navHtmlText = '<ul>' +
+    '<li><a href="../index.html">Home</a></li>' +
+    '<li><a href="penguin_high.html">PENGUIN High Home</a></li>' +
+    '<li><a href="sea_level_rise.html">Sea Level Rise</a></li>' +
+    '<li><a href="penguins.html">Penguins</a></li>' +
+    '<li><a href="permafrost.html">Permafrost</a></li>' +
+    '</ul>'
+document.getElementById("nav").innerHTML = navHtmlText;
+
+
