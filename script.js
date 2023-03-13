1053dcc92efda576ec39df669132785e776f8071
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-12 17:28:17 -0700

    restructure to make main page

--- script.js
+++ script.js
@@ -8,9 +8,13 @@
  * Put the nav bar html text into the HTML
  */
 let navHtmlText = '<ul>' +
-              '<li><a href="index.html">Home</a></li>' +
-              '<li><a href="sudoku.html">Sudoku</a>' +
-              '<li><a href="connectFour.html">Connect Four</a></li>' +
-              '</ul>'
+    '<li><a href="index.html">Home</a></li>' +
+    '<li><a href="research.html">Research</a>' +
+    '<li><a href="projects.html">Projects</a></li>' +
+    '<li><a href="datasets.html">Datasets</a>' +
+    '<li><a href="educational.html">Educational</a></li>' +
+    '<li><a href="cv.html">CV</a></li>' +
+    '<li><a href="publications.html">Publications</a></li>' +
+    '</ul>'
 document.getElementById("nav").innerHTML = navHtmlText;
 
