cc6e400b66ae4b03fdec08c3886e79b8da22a06d
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-19 18:09:19 -0700

    Move datasets before research

--- script.js
+++ script.js
@@ -10,8 +10,8 @@
 let navHtmlText = '<ul>' +
     '<li><a href="index.html">Home</a></li>' +
     '<li><a href="index.html#projects">Projects</a></li>' +
-    '<li><a href="index.html#research">Research</a>' +
     '<li><a href="index.html#datasets">Datasets</a>' +
+    '<li><a href="index.html#research">Research</a>' +
     '<li><a href="index.html#publications">Publications</a></li>' +
     '<li><a href="CV_Rowe_med.pdf">CV</a></li>' +
     '</ul>'
