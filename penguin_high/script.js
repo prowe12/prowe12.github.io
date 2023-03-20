9dcacfc4e02a08206ebb1ed4abaa0d55e2c503db
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2023-03-20 11:24:30 -0700

    Clean up penguin high pages

--- penguin_high/script.js
+++ penguin_high/script.js
@@ -8,8 +8,7 @@
  * Put the nav bar html text into the HTML
  */
 let navHtmlText = '<ul>' +
-    '<li><a href="../index.html">Home</a></li>' +
-    '<li><a href="penguin_high.html">PENGUIN High Home</a></li>' +
+    '<li><a href="penguin_high.html">Home</a></li>' +
     '<li><a href="sea_level_rise.html">Sea Level Rise</a></li>' +
     '<li><a href="penguins.html">Penguins</a></li>' +
     '<li><a href="permafrost.html">Permafrost</a></li>' +
