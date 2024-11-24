3cd5577d54bc605c065ef40323c60bee337ae32f
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2024-11-24 13:23:12 -0800

    Update resumes and CVs

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -5,6 +5,7 @@
 </div>
 
 <div class="flex flex-col items-center mb-80">
-	<a href="CV_Rowe_med.pdf" class="rounded-md px-10 py-2 text-2xl font-medium text-black hover:bg-gray-700 hover:text-white">Data Scientist - CV</a>
-	<a href="CV_Rowe_med.pdf" class="rounded-md px-10 py-2 text-2xl font-medium text-black hover:bg-gray-700 hover:text-white">Software Engineer - Resume</a>
+	<a href="Rowe_Resume_datascience.pdf" class="rounded-md px-10 py-2 text-2xl font-medium text-black hover:bg-gray-700 hover:text-white">Data Scientist - Resume</a>
+	<a href="Rowe_CV_datascience.pdf" class="rounded-md px-10 py-2 text-2xl font-medium text-black hover:bg-gray-700 hover:text-white">Data Scientist - CV</a>
+	<a href="Rowe_Resume_SE.pdf" class="rounded-md px-10 py-2 text-2xl font-medium text-black hover:bg-gray-700 hover:text-white">Software Engineer - Resume</a>
 </div>
